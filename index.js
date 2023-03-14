cc7fce3e81ab98d40235de41cca9db8484d791f0
Tony Hunter <thunter1987@yahoo.com>
2023-03-14 16:41:54 -0500

    "Review Tests Passing"

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Tony Hunter';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
